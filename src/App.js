023a83a908e4c5d1ee281ce8d24cf8376a5901e9
yunanah <bbyl6319@gmail.com>
2022-02-12 11:58:54 +0900

    ✨ Introduce Canvas, List components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [drawings, setDrawings] = useState([]) // 캔버스에 생성된 다각형 배열 state
+
+  // render
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas drawings={drawings} setDrawings={setDrawings} />
+      <List drawings={drawings} />
+    </div>
+  );
+}
+
+// Canvas 컴포넌트 
+function Canvas(props) {
+
+  const { drawings, setDrawings } = props
+
+  let [deleteMode, setDeleteMode] = useState(false) // 삭제하기 모드 state
+  
+
+  return (
+    <div className="container">
+        캔버스
+    </div>
+  );
+}
+
+// List 컴포넌트
+function List(props) {
+
+  const { drawings } = props
+
+  return (
+    <div className="box">
+      리스트
     </div>
   );
 }
